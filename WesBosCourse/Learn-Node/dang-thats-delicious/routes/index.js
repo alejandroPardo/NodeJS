b288869d32b0ee601272fee9984b5230e6d71596
apardo <apardo@corunet.com>
2019-02-14 17:22:39 +0100

    Adds registration and login.

--- WesBosCourse/Learn-Node/dang-thats-delicious/routes/index.js
+++ WesBosCourse/Learn-Node/dang-thats-delicious/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
+const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -24,5 +26,13 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+router.post('/register', 
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
 
 module.exports = router;
